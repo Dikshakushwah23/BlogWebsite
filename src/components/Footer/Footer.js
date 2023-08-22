4f95ea65bb2eabed3d52e3ab86f5dd2ad0b3e220
Diksha Kushwah <diksha.kushwah@lntinfotech.com>
2023-08-22 15:49:27 +0530

    initial commit

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,10 @@
+import React, {memo} from 'react'
+
+function Footer() {
+  return (
+    <div className="card-footer text-muted text-center font-weight-bolder p-5 m-2">
+      Copyright 2023. all rights reserved.
+    </div>
+  )
+}
+export default memo(Footer)
